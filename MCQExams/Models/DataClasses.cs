2db80c24428794e504bb62bb4fa1a8935470df18
amnashahid_sh <amnashahid_sh@hotmail.com>
2020-11-21 11:17:26 +0500

    data classes added

---
+++ MCQExams/Models/DataClasses.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MCQExams.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [Display(Name="Category Name")]
+        public string Name { get; set; }
+        [Display(Name="Description")]
+        public string Description { get; set; }
+    }
+    public class Question
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Qt { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+    }
+    public class Option
+    {
+        public int Id { get; set; }
+        public string  Opt { get; set; }
+        public bool isCorrection { get; set; }
+        public int QuestionId { get; set; }
+        public Question Question { get; set; }
+
+
+    }
+}
